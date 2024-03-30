4734308425ca6b30ebe7351209a54810f87ca3ca
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-03-30 12:12:39 +0000

    fixed nested route

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
 import LoginForm from "../features/users/components/LoginForm2";
 import CreateUserForm from "../features/users/components/CreateUserForm";
-import { Link, useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 
 const LandingPage = () => {
-  const [formType, setFormType] = useState(null);
-  const {id} = useParams()
+  // const [formType, setFormType] = useState(null);
+  // const {id} = useParams()
 
-  useEffect(() => {
-    if (id === 'login' || id === 'register') {
-      setFormType(id)
-    }
+  // useEffect(() => {
+  //   if (id === 'login' || id === 'register') {
+  //     setFormType(id)
+  //   }
   
-  }, [id])
+  // }, [id]) 
+  // const handleOpenForm = (type) => {
+  //   setFormType(type);
+  // };
   
-
-  const handleOpenForm = (type) => {
-    setFormType(type);
-  };
   return (
     <div className="text-center">
       <h1 className="text-3xl md:text-4xl font-bold ">Welcome to Weekly Money Saver App</h1>
@@ -25,30 +24,25 @@ const LandingPage = () => {
         Your personal savings guide! Set your financial goals and track your
         progress with our tailored weekly savings plans.
       </p>
+
       <p>
         Ready to get started?
         <span className="text-teal-600 ml-1 hover:text-teal-800">
-        <Link to="#" onClick={() => handleOpenForm("login")}>
+        <Link to="login">
           Log in
-        </Link>{" "}
+        </Link>
         </span>
         or
         <span className="text-teal-600 ml-1 hover:text-teal-800">
-        <Link to="#" onClick={() => handleOpenForm("register")}>
+        <Link to="register">
           Register
         </Link>
         </span>
       </p>
-      {formType === "login" && (
-        <>
-          <LoginForm />
-          {/* <p>
-            if not register, click{" "}
-            <Link to="#" onClick={handleOpenForm("register")}></Link>
-          </p> */}
-        </>
-      )}
-      {formType === "register" && <CreateUserForm />}
+      
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
