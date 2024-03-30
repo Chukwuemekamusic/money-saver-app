84f72d18a7a60ecc0f572b7649052e06e43c9057
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-03-30 12:16:02 +0000

    updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,37 @@
 import React, { useState, useEffect } from "react";
-import LoginForm from "../features/users/components/LoginForm2";
-import CreateUserForm from "../features/users/components/CreateUserForm";
 import { Link, Outlet, useParams } from "react-router-dom";
 
 const LandingPage = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl md:text-4xl font-bold ">
+        Welcome to Weekly Money Saver App
+      </h1>
+      <p>
+        Your personal savings guide! Set your financial goals and track your
+        progress with our tailored weekly savings plans.
+      </p>
+
+      <p>
+        Ready to get started?
+        <span className="text-teal-600 ml-1 hover:text-teal-800">
+          <Link to="login">Log in</Link>
+        </span>
+        or
+        <span className="text-teal-600 ml-1 hover:text-teal-800">
+          <Link to="register">Register</Link>
+        </span>
+      </p>
+
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
+
   // const [formType, setFormType] = useState(null);
   // const {id} = useParams()
 
@@ -16,35 +44,3 @@ const LandingPage = () => {
   // const handleOpenForm = (type) => {
   //   setFormType(type);
   // };
-  
-  return (
-    <div className="text-center">
-      <h1 className="text-3xl md:text-4xl font-bold ">Welcome to Weekly Money Saver App</h1>
-      <p>
-        Your personal savings guide! Set your financial goals and track your
-        progress with our tailored weekly savings plans.
-      </p>
-
-      <p>
-        Ready to get started?
-        <span className="text-teal-600 ml-1 hover:text-teal-800">
-        <Link to="login">
-          Log in
-        </Link>
-        </span>
-        or
-        <span className="text-teal-600 ml-1 hover:text-teal-800">
-        <Link to="register">
-          Register
-        </Link>
-        </span>
-      </p>
-      
-      <div>
-        <Outlet />
-      </div>
-    </div>
-  );
-};
-
-export default LandingPage;
