a8b939276b204c0762110a2eb21e2abe5f4ea087
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-04-08 16:23:54 +0100

    update after cleanup2

--- src/features/savings/SavingPlanDetail.js
+++ src/features/savings/SavingPlanDetail.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import PaymentButtons from "../../components/PaymentButtons";
 import SavingSummary from "../../components/SavingSummary";
 import { date } from "../../utils/savingsUtils.js";
@@ -16,13 +16,17 @@ const SavingPlanDetail = () => {
   const { navigateHome } = useCustomNavigation();
   const { id } = useParams();
   // console.log("id", id);
-  const savingsData = useSelector(selectSavingDetail) ?? [];
-  console.log("savingsData", savingsData);
+  const sData = useSelector(selectSavingDetail);
+  const savingsData = useMemo(() => sData ?? [], [sData]);
 
+  // const savingsData = useSelector(selectSavingDetail) ?? [];
+  // console.log("savingsData", savingsData);
+
+  // eslint-disable-next-line
   const savings = savingsData.filter((saving) => saving.id == id)[0] ?? "";
-  console.log("savings from data", savings);
+  // console.log("savings from data", savings);
 
-  // #TODO I have to fix page to directly get detail from api ***Fixed!! 
+  // #TODO I have to fix page to directly get detail from api ***Fixed!!
   // though this ensures nobody gets to the page withut using the dashboard
   useEffect(() => {
     if (!id || savingsData === null || savingsData.length === 0 || !savings) {
@@ -31,7 +35,7 @@ const SavingPlanDetail = () => {
   }, [id, savingsData, savings, navigateHome]);
 
   const targetAmount = savings.amount;
-  const { amount_list, amount, savings_name, date_created } = savings;  
+  const { amount_list, amount, savings_name, date_created } = savings;
 
   return (
     <div>
@@ -39,23 +43,42 @@ const SavingPlanDetail = () => {
         // flex flex-col items-center
         <div className=" ">
           <header className="text-center">
-            <h1 className="font-bold text-teal-700 text-center text-5xl">MONEY SAVING CHART</h1>
+            <h1 className="font-bold text-teal-700 text-center text-5xl">
+              MONEY SAVING CHART
+            </h1>
             {/* <h3>USE THIS CHART TO SAVE AN EXTRA £{sumNumbers} IN 2024</h3> */}
-            <h3 className="font-semibold text-gray-700 text-center text-2xl" >USE THIS CHART TO SAVE AN EXTRA £{parseInt(amount)} WITHIN A YEAR</h3>
-            </header>
-            {targetAmount && (
-              <div className="font-semibold text-xl flex justify-between items-center gap-10 p-3 px-6 bg-white/35 rounded-full shadow-md md:w-3/4 mx-auto hover:shadow-emerald-100">
-                <h2 className="italic"><span className="block text-teal-900 font-bold not-italic "> Saving Plan: </span> {savings_name.toUpperCase()}</h2>
-                <p className=""><span className="block text-teal-900 font-bold"> Target: </span>£{amount}</p>
-                {/* # TODO fix date properly */}
-                <p><span className="block text-teal-900 font-bold"> Date: </span>{date(date_created)}</p>
-              </div>
-            )}
-         
+            <h3 className="font-semibold text-gray-700 text-center text-2xl">
+              USE THIS CHART TO SAVE AN EXTRA £{parseInt(amount)} WITHIN A YEAR
+            </h3>
+          </header>
+          {targetAmount && (
+            <div className="font-semibold text-xl flex justify-between items-center gap-10 p-3 px-6 bg-white/35 rounded-full shadow-md md:w-3/4 mx-auto hover:shadow-emerald-100">
+              <h2 className="italic">
+                <span className="block text-teal-900 font-bold not-italic ">
+                  {" "}
+                  Saving Plan:{" "}
+                </span>{" "}
+                {savings_name.toUpperCase()}
+              </h2>
+              <p className="">
+                <span className="block text-teal-900 font-bold"> Target: </span>
+                £{amount}
+              </p>
+              {/* # TODO fix date properly */}
+              <p>
+                <span className="block text-teal-900 font-bold"> Date: </span>
+                {date(date_created)}
+              </p>
+            </div>
+          )}
 
           <div className="flex-container justify-center">
             <PaymentButtons noList={amount_list} setNoList={setAmountList} />
-            <SavingSummary amount_list={amount_list} id={id} target={targetAmount}/>
+            <SavingSummary
+              amount_list={amount_list}
+              id={id}
+              target={targetAmount}
+            />
           </div>
         </div>
       )}
