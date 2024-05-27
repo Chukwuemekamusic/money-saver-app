a8061d111fea14ab1c10831c8bc3b61ddde27f14
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-05-27 13:08:22 +0100

    first namen error fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import CreateUserForm from "./features/users/components/CreateUserForm";
 import Home from "./components/HomePage";
 import LoginForm2 from "./features/users/components/LoginForm2";
-import { Route, createRoutesFromElements } from "react-router-dom";
+import { Route, createRoutesFromElements, Navigate } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import SavingPlanDetail from "./features/savings/SavingPlanDetail";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./RootLayout";
 import { NotFoundPage } from "./components/NotFoundPage";
 import ProtectedRoute from "./ProtectedRoute";
+import { useSelector } from "react-redux";
+import { SelectUserInfo } from "./features/auth/authSlice";
 
 function App() {
+
+  const userInfo = useSelector(SelectUserInfo)
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<RootLayout />}>
@@ -21,8 +25,8 @@ function App() {
         {/* loader={homeLoader(dispatch)} */}
 
         <Route element={<ProtectedRoute />}>
-          <Route index element={<Home />} path="/" />
-          <Route element={<SavingPlanDetail />} path="/saving_plan/:id" />
+          <Route index element={userInfo ? <Home /> : <Navigate to='/landing' />} path="/" />
+          <Route element={userInfo ? <SavingPlanDetail /> :  <Navigate to='/landing' />} path="/saving_plan/:id" />
         </Route>
 
         <Route element={<NotFoundPage />} path="*" />
