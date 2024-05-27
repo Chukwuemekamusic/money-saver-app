cf4d50ccea41c7cb5d8340b46bca7fe96386c6c8
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-05-27 13:56:18 +0100

    updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,29 +3,15 @@ import SavingPlanList from "../features/savings/SavingPlanList";
 import { useSelector } from "react-redux";
 import { selectAllSavings } from "../features/savings/savingsSlice";
 import { SelectUserInfo } from "../features/auth/authSlice";
-// import useCheckAuth from "../features/auth/utils/useCheckAuth";
-// import { listSavingPlan } from "../features/savings/savingAction";
-// import { useRef } from "react";
 
 const Home = () => {
-  const { isLoading, isSuccess, savings, error } =
-    useSelector(selectAllSavings);
-  const userInfo = useSelector(SelectUserInfo);
-  console.log('user', userInfo);
-  console.log('first_name', userInfo.first_name);
-  console.log('first_name', userInfo);
-  // const checkAuth = useCheckAuth();
-  // const dispatch = useDispatch();
-  // const inputRef = useRef(null)
-  // // const focusOn = () => inputRef?.current?.focus()
-  // // focusOn()
-  // useEffect(() => {
-  //   // checkAuth();
-  //   dispatch(listSavingPlan());
-  // }, []);
+  const { isLoading, isSuccess, savings, error } = useSelector(selectAllSavings);
 
-  // const date = new Date()
-  //   const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
+  const userInfo = useSelector(SelectUserInfo);
+  console.log("user", userInfo);
+  console.log("first_name", userInfo?.first_name);
+  console.log("first_name", userInfo);
+
   return (
     <div className="text-center">
       {isLoading || !userInfo ? (
@@ -35,7 +21,7 @@ const Home = () => {
           <h2 className="text-3xl md:text-4xl mb-4">
             Hi{" "}
             <span className="font-bold text-teal-700 capitalize">
-              {userInfo.first_name}
+              {userInfo?.first_name}
             </span>
             , Welcome to Your Savings Dashboard
           </h2>
