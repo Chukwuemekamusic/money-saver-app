1144cc4e00fe9823893a23178bc27f7ff75e131b
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-08-11 13:13:15 +0100

    update after gauth

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet, useLocation} from "react-router-dom";
 const LandingPage = () => {
   const location = useLocation();
   
-  console.log('Current path:', location.pathname);
+  // console.log('Current path:', location.pathname);
   return (
     <div className="container mx-auto px-4 py-4">
       <h1 className="text-3xl md:text-4xl font-bold text-center mb-2 ">
